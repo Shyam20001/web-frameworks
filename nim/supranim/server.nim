18fb27f22fd8c7fe0150e0b5fc235b4147cea758
George Lemon <georgelemon@protonmail.com>
2026-03-12 03:55:23 +0200

    add dep | simplify starter
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- nim/supranim/server.nim
+++ nim/supranim/server.nim
@@ -1,46 +1,22 @@
-#
-# This is the main file for the Supranim application.
-#
-# It initializes the application, loads configurations,
-# and sets up the necessary services and middlewares.
-#
+import std/[httpcore, macros, macrocache, options]
+import pkg/kapsis/[cli, runtime]
 import pkg/supranim
+import pkg/supranim/controller
+import pkg/supranim/core/[request, router, response]
 
-#
-# Init core modules using `init` macro
-#
-App.init(skipLocalConfig = true) do:
-  App.configs = newOrderedTable[string, Document]()
-  App.configs["server"] = yaml("").toJson
 
-# Define CLI commands for the application
-import pkg/kapsis/[runtime, cli]
-proc startCommand(v: Values) =
-  ## Kapsis `init` command handler
-  initStartCommand(v, createDirs = false)
+include ./routes
+import ./controller/[pages, errors]
 
-  App.configs["server"].contents = %*{
-    "port": int(v.get("--port").getPort)
-  }
+initApplication()
+initHttpRouter()
 
-App.cli do:
-  start path(directory), port(--port):
-    ## Init the app with the given installation path
+proc startupCallback() {.gcsafe.} =
+  {.gcsafe.}:
+    initRouter()
+    Router.errorHandler(Http404, get4xx)
 
-#
-# Initialize available Service Providers.
-#
-# Configuration files are defined as YAML in the
-# `config/` directory.
-#
-App.services do:
-  # initialize your service providers here
-  discard
+App.configs = newOrderedTable[string, Document]()
+App.configs["server"] = yaml("port: 3000").toJson
 
-#
-# Starts the application. This will start the HTTP
-# server and listen for incoming requests.
-#
-# The application will be available at the specified port.
-#
-App.run()
+App.run
