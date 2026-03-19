17fdbb42d74edf637ade0eb5be29ee49d9965849
Seven Du <5564821+medz@users.noreply.github.com>
2026-03-20 02:33:33 +0800

    refactor(dart): use spry osrv exports

--- dart/spry/server.dart
+++ dart/spry/server.dart
@@ -1,8 +1,8 @@
 import 'dart:io';
 import 'dart:isolate';
 
-import 'package:osrv/osrv.dart' show Server;
-import 'package:osrv/runtime/dart.dart' show serve;
+import 'package:spry/osrv.dart' show Server;
+import 'package:spry/osrv/dart.dart' show serve;
 import 'package:spry/spry.dart' show HttpMethod, Response, Spry;
 
 final app = Spry(
